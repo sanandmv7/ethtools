5322fa4eb2e9aed68abc2b9c15934c2ea7d6e8ec
sanandmv7 <56066399+sanandmv7@users.noreply.github.com>
2023-10-21 19:53:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { ethers } from 'ethers';
 
 function App() {
+  const [weiAmount, setWeiAmount] = useState("0");
+  const [etherAmount, setEtherAmount] = useState("0");
+
+  function handleWeiUpdate(e) {
+    setWeiAmount(e.target.value);
+    console.log(`handleWeiUpdate: ${e.target.value}`);
+    try{
+      const ethValue = ethers.formatEther(e.target.value);
+      setEtherAmount(ethValue);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  function handleEthUpdate(e) {
+    setEtherAmount(e.target.value);
+    console.log(`handleEthUpdate: ${e.target.value}`);
+    try{
+      const weiValue = ethers.parseUnits(e.target.value,"ether");
+      console.log(`weiValue: ${weiValue}`);
+      setWeiAmount(weiValue);
+      console.log(`weiAmount: ${weiAmount}`);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content_wrapper">
+      <h1 align="center">EthTools</h1>
+      <div className="request container">
+        <h2>Unit Converter</h2>
+        <br />
+        <form>
+          <label>
+            Wei:
+            <br />
+            <input
+              type="text"
+              value={weiAmount}
+              onChange={(e) => handleWeiUpdate(e)}
+            />
+          </label>
+          <br />
+          <label>
+            Ether:
+            <br />
+            <input
+              type="text"
+              value={etherAmount}
+              onChange={(e) => handleEthUpdate(e)}
+            />
+          </label>
+          <br />
+        </form>
+      </div>
     </div>
   );
 }
